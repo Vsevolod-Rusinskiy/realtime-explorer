5aca6ac10a674e6ae26bd706d80c5a442ee8f0e3
Vsevolod-Rusinskiy <leto99999@gmail.com>
2025-05-07 15:20:53 +0300

    update: enhance deployment script to include environment variable loading and display project directory contents

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,17 @@ nvm use 20.9.0
 
 # 2. Переход в проект
 cd /var/www/realtime-explorer/
+echo "PWD: $(pwd)"
+ls -l .env docker-compose.yml
 
 # 3. Обновление кода
 git pull origin main
 
 # 4. Docker Compose: билд и запуск
 # (убедись, что docker-compose.yml настроен на порты 3003 и 5433)
+set -a
+source .env
+set +a
 docker-compose pull
 docker-compose build
 
